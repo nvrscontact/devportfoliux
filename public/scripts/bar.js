1a8bacb05423e87dc404e2ff2d46d4edad83baf8
nvrsunk <nvrsdark@gmail.com>
2025-08-23 23:17:13 +0200

    g

--- public/scripts/bar.js
+++ public/scripts/bar.js
@@ -10,3 +10,19 @@ window.onload = function () {
         scrollContainer.scrollLeft = offset;
     }
 };
+
+function getGreeting(){
+    const hour = new Date().getHours();
+    let greeting = '';
+
+    if (hour >= 6 && hour < 12) {
+        greeting = 'Good Morning';
+    } else if (hour >= 12 && hour < 18) {
+        greeting = 'Good Afternoon';
+    } else {
+        greeting = 'Good Night';
+    }
+    document.getElementById('greeting').textContent = greeting;    
+}
+
+getGreeting();
